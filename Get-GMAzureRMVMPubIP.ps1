20b352c387c1227a4430f9f4a4090229c4555bb3
Gabriel McColl <gabriel@cloudconfusion.co.uk>
2017-09-08 17:17:04 +0100

    added comments

--- Get-GMAzureRMVMPubIP.ps1
+++ Get-GMAzureRMVMPubIP.ps1
@@ -1,6 +1,6 @@
 
 function Get-GMAzureRMVMPubIP ($resourcegroupname,$vmname) {
-
+#function will bring back the public IP of a VM 1 NIC only
 $vm = get-azurermvm  -Name $vmname -ResourceGroupName $resourcegroupname 
 $vmnicname = $vm.NetworkProfile.NetworkInterfaces.ID.split("/")[8]
 $nic = Get-AzureRmNetworkInterface -Name $vmnicname -ResourceGroupName $resourcegroupname
